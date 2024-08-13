1b81aeac6619dba5b85b1e78d0db5be76def2968
NPUBJIEP\npubj <npubjiep@gmail.com>
2024-08-13 10:18:07 +0500

    add handlers, add request models

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
+from typing_extensions import List
 
 app = FastAPI()
 
@@ -10,42 +11,51 @@ db_client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = db_client.user_balance_db
 
 
-class CreateUserBalance(BaseModel):
-    user_id: str = Field(..., example="user123")
-    balance: float = Field(
-        default=0.0, ge=0.0, example=100.0
-    )  # ge=0.0 гарантирует, что баланс не будет отрицательным
-
-
-class UserBalance(BaseModel):
-    user_id: str
-    amount: float
-
-
-class TransferBalance(BaseModel):
+class BalanceTransferRequest(BaseModel):
     from_user_id: str
     to_user_id: str
     amount: float
 
 
-@app.get("/")
-async def credit_balance():
-    return {"ok"}
+class BalanceAddRequest(BaseModel):
+    user_id: str
+    amount: float
+
+
+class BalanceWithdrawRequest(BaseModel):
+    user_id: str
+    amount: float
+
+
+class CreateUserRequest(BaseModel):
+    user_id: str
+    amount: float
 
 
 @app.post("/users/")
-async def create_user_balance(data: CreateUserBalance):
-    # Создание новой записи пользователя
+async def create_user(data: CreateUserRequest):
     new_user = {"user_id": data.user_id, "balance": data.balance}
     await db.balances.insert_one(new_user)
     return {"success"}
 
 
-@app.get("/users/{id}")
-async def get_user_balance(id: str):
-    print(id)
-    # Создание новой записи пользователя
-    user = {"user_id": id}
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    user = {"user_id": user_id}
     await db.balances.find_one(user)
-
     return {"success"}
+
+
+@app.post("/balance/add")
+async def add_balance(data: BalanceAddRequest):
+    pass
+
+
+@app.post("/balance/withdraw")
+async def add_balance(data: BalanceWithdrawRequest):
+    pass
+
+
+@app.post("/balance/transfer")
+async def add_balance(data: BalanceTransferRequest):
+    pass
