66f2d235262df1602a4c3d6887f04010068cc4c1
NPUBJIEP\npubj <npubjiep@gmail.com>
2024-08-13 09:25:13 +0500

    install black

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ app = FastAPI()
 db_client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = db_client.user_balance_db
 
+
 class CreateUserBalance(BaseModel):
     user_id: str = Field(..., example="user123")
-    balance: float = Field(default=0.0, ge=0.0, example=100.0)  # ge=0.0 гарантирует, что баланс не будет отрицательным
+    balance: float = Field(
+        default=0.0, ge=0.0, example=100.0
+    )  # ge=0.0 гарантирует, что баланс не будет отрицательным
 
 
 class UserBalance(BaseModel):
@@ -35,16 +38,14 @@ async def create_user_balance(data: CreateUserBalance):
     # Создание новой записи пользователя
     new_user = {"user_id": data.user_id, "balance": data.balance}
     await db.balances.insert_one(new_user)
-    return {'success'}
+    return {"success"}
 
 
 @app.get("/users/{id}")
-async def get_user_balance(id:str):
+async def get_user_balance(id: str):
     print(id)
     # Создание новой записи пользователя
     user = {"user_id": id}
     await db.balances.find_one(user)
 
-    return {'success'}
-
-
+    return {"success"}
